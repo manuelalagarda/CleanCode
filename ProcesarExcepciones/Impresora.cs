38f4062aef209820f262f0088990c517a44bcff6
Manuel Alagarda <manuel.alagarda@nunsys.com>
2016-10-04 08:51:29 +0200

    Primera versión - Excepciones

---
+++ ProcesarExcepciones/Impresora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcesarExcepciones
+{
+    public enum TipoRespuesta
+    {
+        Ok,
+        ErrorDevice,
+        ErrorPrinting
+    }
+
+    public class Impresora
+    {
+        public TipoRespuesta Open()
+        {
+            return TipoRespuesta.Ok;
+        }
+
+        public TipoRespuesta Print(string texto)
+        {
+            System.Console.WriteLine(texto);
+            //return TipoRespuesta.ErrorPrinting;
+            return TipoRespuesta.Ok;
+        }
+
+        public TipoRespuesta Close()
+        {
+            return TipoRespuesta.Ok;
+        }
+    }
+}
---
+++ ProcesarExcepciones/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcesarExcepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Impresora impresora = new Impresora();
+            TipoRespuesta respuesta = TipoRespuesta.Ok;
+
+            respuesta = impresora.Open();
+            if (respuesta == TipoRespuesta.Ok)
+            {
+                respuesta = impresora.Print("Hola Mundo!!");
+                switch (respuesta)
+                {
+                    case TipoRespuesta.Ok:
+                        System.Console.WriteLine("Impresión correcta");
+                        break;
+                    case TipoRespuesta.ErrorDevice:
+                        System.Console.WriteLine("Error al acceder al dispositivo");
+                        break;
+                    case TipoRespuesta.ErrorPrinting:
+                        System.Console.WriteLine("Error al imprimir");
+                        break;
+                }
+
+                respuesta = impresora.Close();
+                if (respuesta != TipoRespuesta.Ok)
+                {
+                    System.Console.WriteLine("Error al acceder dispositivo");
+                }
+
+            } else
+            {
+                System.Console.WriteLine("Error al acceder dispositivo");
+            }
+           
+
+            System.Console.ReadKey();
+
+        }
+    }
+}
